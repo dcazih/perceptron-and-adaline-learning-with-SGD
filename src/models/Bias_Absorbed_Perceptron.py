b2e5daf7eb6ef3eccb4576b100855f9c05f3c8d0
Dmitri Azih <dmitriazih@gmail.com>
2025-02-18 21:03:57 -0700

    moved to models

--- src/models/Bias_Absorbed_Perceptron.py
+++ src/models/Bias_Absorbed_Perceptron.py
@@ -77,13 +77,13 @@ y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', 0, 1)
 X = df.iloc[0:100, [0, 2]].values
 
-# Create the bias absorbed Perceptron and train it
-bA_ppn = Perceptron_bAbsorbed(eta=0.1, n_iter=10)
-bA_ppn.fit(X, y)
-
 # Create regular Perceptron and train it
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
+
+# Create the bias absorbed Perceptron and train it
+bA_ppn = Perceptron_bAbsorbed(eta=0.1, n_iter=10)
+bA_ppn.fit(X, y)
 
 # Plot the graphs to show comparison
 plt.figure(figsize=(8,5))
